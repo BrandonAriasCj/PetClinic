c13bdc53aac65cc8142ddc9bfcd4891a13d96c72
Danna <danna.mendoza@tecsup.edu.pe>
2025-05-29 13:30:54 -0500

    creating owner and ownernotfoundexception

---
+++ src/main/java/com/tecsup/petclinic/exception/OwnerNotFoundExecption.java
@@ -0,0 +1,11 @@
+package com.tecsup.petclinic.exception;
+
+public class OwnerNotFoundExecption extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public OwnerNotFoundExecption(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+}
