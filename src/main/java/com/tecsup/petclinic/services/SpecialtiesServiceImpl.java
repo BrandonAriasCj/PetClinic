b008ae51ba998e1fb606a9f2f435bcda03aa1da8
Danna <danna.mendoza@tecsup.edu.pe>
2025-05-29 14:27:44 -0500

    cambios service

--- src/main/java/com/tecsup/petclinic/services/SpecialtiesServiceImpl.java
+++ src/main/java/com/tecsup/petclinic/services/SpecialtiesServiceImpl.java
@@ -1,4 +1,5 @@
 package com.tecsup.petclinic.services;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -7,15 +8,60 @@ import org.springframework.stereotype.Service;
 
 import com.tecsup.petclinic.entities.Speciality;
 import com.tecsup.petclinic.exception.SpecialityNotFoundException;
-
+import com.tecsup.petclinic.repositories.SpecialtyRepository;
 
 @Service
 @Slf4j
-public SpecialtiesServiceImpl implements SpecialtiesService{
+public class SpecialtiesServiceImpl implements SpecialtiesService {
 
-    private final Specialties SpecialtyRepository;
+    private final SpecialtyRepository SpecialtyRepository;
 
-    public SpecialtiesServiceImpl(SpecialtyRepository specialtyRepository){
+    public SpecialtiesServiceImpl(SpecialtyRepository SpecialtyRepository) {
+        this.SpecialtyRepository = SpecialtyRepository;
+    }
 
+    @Override
+    public Speciality create(Speciality Speciality) {
+        return SpecialtyRepository.save(Speciality);
+    }
+
+    @Override
+    public Speciality update(Speciality Speciality) {
+        return SpecialtyRepository.save(Speciality);
+    }
+
+    @Override
+    public void delete(Integer id) throws SpecialityNotFoundException {
+        Speciality Speciality = findById(id);
+        SpecialtyRepository.delete(Speciality);
+    }
+
+    @Override
+    public Speciality findById(Integer id) throws SpecialityNotFoundException {
+        Optional<Speciality> Speciality = SpecialtyRepository.findById(id);
+
+        if (!Speciality.isPresent())
+            throw new SpecialityNotFoundException("Record not found...!");
+
+        return Speciality.get();
+    }
+
+    @Override
+    public List<Speciality> findByName(String name) {
+        List<Speciality> specialties = SpecialtyRepository.findByName(name);
+        specialties.forEach(s -> log.info("" + s));
+        return specialties;
+    }
+
+    @Override
+    public List<Speciality> findByOffice(String office) {
+        List<Speciality> specialties = SpecialtyRepository.findByOffice(office);
+        specialties.forEach(s -> log.info("" + s));
+        return specialties;
+    }
+
+    @Override
+    public List<Speciality> findAll() {
+        return SpecialtyRepository.findAll();
     }
 }
